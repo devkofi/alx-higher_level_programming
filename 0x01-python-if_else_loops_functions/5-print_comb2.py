809259e8f4200ae3cc3537a796c9be03c3046506
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-21 15:31:38 +0000

    Check for lowercase

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-text = ""
-for value in range(0, 100):
+text = []
+for value in range(100):
     if(value < 10):
-        text = text + f"0{value}, "
-    elif(value == 99):
-        text = text + f"{value}\n"
+        text.append(f"0{value}")
     else:
-        text = text + f"{value}, "
-print(text.format(), end="")
+        text.append(f"{value}")
+print(text, end="")
