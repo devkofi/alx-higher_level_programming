acabf15c8ad017d885a086450378074a3862190a
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-21 17:03:29 +0000

    Print ASCII alphabet in reverse

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+txt = ""
+for alphabet in range(97, 123):
+    if(alphabet % 2 != 0):
+        txt += chr(alphabet - 32)
+    else:
+        txt += chr(alphabet)
+print(txt[::-1].format(), end="")
