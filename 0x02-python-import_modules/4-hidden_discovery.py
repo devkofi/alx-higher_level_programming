462b47cf0be13ff91dbb18fd731dc2fa1520c041
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-24 00:09:05 +0000

    Read from compiled file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def print_magic_names():
+    items = dir(hidden_4)
+    length = len(items)
+    for item in range(length - 3, length):
+        print("{}".format(items[item]), end="\n")
+
+
+if __name__ == "__main__":
+    import sys
+    print_magic_names()
