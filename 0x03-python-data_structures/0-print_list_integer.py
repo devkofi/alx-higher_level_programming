7ab2514901f9e76e66251e34f1dcefcd9f022bd6
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-25 19:49:47 +0000

    Add integer requirement

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,7 @@
 
 def print_list_integer(my_list=[]):
     for item in range(len(my_list)):
-        print("{}".format(my_list[item]))
+        print("{}".format(int(my_list[item])))
 
 if __name__ == "__main__":
     print_list_integer()
