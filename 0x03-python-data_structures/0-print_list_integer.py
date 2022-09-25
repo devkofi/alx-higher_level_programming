ebece069c95bcb9674244f1b6cf26b08fb82a46a
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-25 19:02:38 +0000

    Remove additional newline

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,7 @@
 
 def print_list_integer(my_list=[]):
     for item in my_list:
-        txt = str(item).format()
-        print("{}".format(txt), end="\n")
+        print("{}".format(item))
 
 
 if __name__ == "__main__":
