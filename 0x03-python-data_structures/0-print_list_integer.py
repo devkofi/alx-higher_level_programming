626b15cabdafa813b2e4862c04c190f875bb2a05
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-25 19:23:22 +0000

    Add formatted string

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,4 @@
 
 def print_list_integer(my_list=[]):
     for item in my_list:
-        print(str(item).format(), end="\n")
-
-
-if __name__ == "__main__":
-    print_list_integer()
+        print("{}".format(item))
