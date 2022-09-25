9ffa1b46d9eed2d004759c2ccce22071cfb1abc2
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-25 18:57:27 +0000

    Add none executable clause

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
     for item in my_list:
         txt = str(item).format()
         print("{}".format(txt), end="\n")
+
+
+if __name__ == "__main__":
+    print_list_integer()
