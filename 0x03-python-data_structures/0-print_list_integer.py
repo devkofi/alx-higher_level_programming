db5e068b8cfc1fe46eb24d9b80c35a89f2aa10a5
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-25 21:00:01 +0000

    Optimize print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    for item in range(len(my_list)):
-        print("{}".format(int(my_list[item])), end="\n")
-
-
-if __name__ == "__main__":
-    print_list_integer()
+    for item in sorted(my_list):
+        print(item)
