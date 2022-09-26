b9e9785578790721c432d96cb392c8e88c12edc1
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-26 16:36:19 +0000

    Remove executable restriction

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,3 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for item in range(len(my_list)):
         print("{:d}".format(my_list[item]))
-
-
-if __name__ == "__main__":
-    print_reversed_list_integer()
