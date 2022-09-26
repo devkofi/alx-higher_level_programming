9f0428dbe94753d7adf9ee87333a26019f447052
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-26 16:36:40 +0000

    Deleted file

--- 0x03-python-data_structures/0-print_list_integer copy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for item in sorted(my_list):
-        print("{:d}".format(item))
-
-
-if __name__ == "__main__":
-    print_list_integer()
