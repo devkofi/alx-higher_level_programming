90bafb7d9b9d3cdda75a4b2c15373d6cdfd24740
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-27 05:05:11 +0000

    Modify item returned

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     count = 0
-    item = ''
+    item = sentence[0]
     if(len(sentence) > 0):
         for word in sentence:
             for index in range(len(word)):
                 count += 1
-        this_tuple = (count, sentence[0])
+        this_tuple = (count, item)
         return this_tuple
     else:
         return None
