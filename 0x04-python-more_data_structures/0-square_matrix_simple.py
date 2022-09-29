d3920c466320142f4c25d4cfc83a5d5afa87b34c
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-09-29 01:49:38 +0000

    Square Matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    res = []
+    for item in matrix:
+        item = list(map(lambda x: x*x, item))
+        res.append(item)
+    return res
